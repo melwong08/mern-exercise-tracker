506e40d56511069dc922df2cdff5ae6a70cc3d79
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-02-11 13:32:17 -0800

    Build routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,4 +9,10 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req,res) => {
     const username = req.body.username;
+    const newUser = new User({username});
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 })
+
+module.exports = router;
